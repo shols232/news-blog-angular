f7217bd710c733b962d49e579518dda9922ea9c2
shols232 <akinsolaademola232@gmail.com>
2021-01-11 16:59:41 +0100

    changed nav color

--- src/app/container/sections/home/home.component.ts
+++ src/app/container/sections/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { FullSectionPost } from '../shared/section-post.model';
 import { SectionsService } from '../shared/sections.service';
 import { PreviewPost } from './preview-post.model'
@@ -8,9 +9,10 @@ import { PreviewPost } from './preview-post.model'
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   latest_post: FullSectionPost;
   headlines: FullSectionPost[] = [];
+  getPostsSub: Subscription;
   entertainment_latest: FullSectionPost[] = [];
   business_latest: FullSectionPost[] = [];
   politics_latest: FullSectionPost[] = [];
@@ -23,15 +25,22 @@ export class HomeComponent implements OnInit {
   }
 
   getPosts(){
-    this.sectionsService.getHomePosts().subscribe(data => {
-      console.log(data)
-      this.latest_post = data.latest
+    this.getPostsSub = this.sectionsService.getHomePosts().subscribe(data => {
+      if(data.latest){
+        this.latest_post = data.latest
+      }else{
+        this.latest_post = null
+      }
       this.entertainment_latest = data.entertainment
       this.business_latest = data.business
       this.politics_latest = data.politics
       this.humanity_latest = data.humanity
       this.headlines = data.headlines
     })
+  }
+
+  ngOnDestroy(){
+    this.getPostsSub.unsubscribe()
   }
 
 }
