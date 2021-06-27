fa1136250a47a055c6c074d5fc5c397b48dcb757
shols232 <akinsolaademola232@gmail.com>
2021-06-27 16:46:54 +0100

    fix error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Router, NavigationEnd } from '@angular/router';
 import { environment } from '../environments/environment'
 import { Title } from '@angular/platform-browser';
 
-declare var gtag
+declare var gtag: Function
 
 @Component({
   selector: 'app-root',
@@ -18,19 +18,17 @@ export class AppComponent implements OnInit {
   title = 'SkrapNews';
 
   constructor(private titleService: Title, private authService: AuthService, private cookieService: CookieService, router: Router){
-
-    this.titleService.setTitle('Home');
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=' + environment.analyticsCode;
+    document.head.prepend(script);
+    // this.titleService.setTitle('Home');
     const navEndEvent$ = router.events.pipe(
       filter(e => e instanceof NavigationEnd)
     );
     navEndEvent$.subscribe((e: NavigationEnd) => {
       gtag('config', 'MY_ID', {'page_path':e.urlAfterRedirects});
     });
-
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=' + environment.analyticsCode;
-    document.head.prepend(script);
   }
 
   ngOnInit(){
